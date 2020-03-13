41f2067313fa122bace2b99ff3417c40f2fd4222
Sunil Karki <karkisunil1200@gmail.com>
2020-03-13 10:48:00 -0500

    Get request for project works

--- api/projectRouter.js
+++ api/projectRouter.js
@@ -1,5 +1,21 @@
 const express = require('express');
 
+const Hubs = require('../data/helpers/projectModel');
+
 const router = express.Router();
+
+router.get('/', (req, res) => {
+  Hubs.get()
+    .then(hubs => {
+      if (hubs) {
+        res.status(200).json(hubs);
+      } else {
+        res.status(400).json({message: 'Data not found'});
+      }
+    })
+    .catch(error => {
+      res.status(500).json({message: 'Invalid credentials'});
+    });
+});
 
 module.exports = router;
